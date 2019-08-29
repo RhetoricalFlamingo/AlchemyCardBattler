7ad83033366ec8fd36e625eb882c8aaa063c1d40
Kevin Ray <kr1995@nyu.edu>
2019-08-29 19:14:54 -0400

    Error Fix

--- AlchemyCardBattler/Assets/Scripts/CardInit.cs
+++ AlchemyCardBattler/Assets/Scripts/CardInit.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 
 public class CardInit : MonoBehaviour
---
+++ AlchemyCardBattler/Assets/Scripts/Mouse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Mouse : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+     
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag("MyCard"))
+            print (hit.collider.name);
+        }
+    }
+}
+
