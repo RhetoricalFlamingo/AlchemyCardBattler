2cbab5cd5e9f9ee31e54123adf128c9f6422fe86
akoshy21 <ak6429@nyu.edu>
2019-08-29 19:50:50 -0400

    basic start

---
+++ AlchemyCardBattler/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnManager : MonoBehaviour
+{
+    int turnState = 0; // 0 = draw, 1 = play ingredient, 2 = take action
+    bool playerOne = true;
+    public GameObject turnDivider;
+
+    public void Draw ()
+    {
+
+    }
+
+    public void PlayIngredient()
+    {
+
+    }
+
+    public void TakeAction()
+    {
+
+    }
+
+    private void Start()
+    {
+        
+    }
+}
